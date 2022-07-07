14e9b9ee1b12bb629d514782b81b1fb699d96297
dmitry <shudv@yandex.ru>
2022-07-07 11:48:19 +0300

    relocate articles_controlle to contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,11 @@
 class ContactsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @params = "#{params[:message]} --- #{params[:address]}"
+    
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   
-  get 'home/contacts'
   
-  post 'home/get_message'
-  
+  resource :contacts, only: [:new, :create]
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
