4e12ab589134a4d0740b13d8a90375b4d2dd6538
dmitry <shudv@yandex.ru>
2022-07-13 11:04:50 +0300

    add comments on article template

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,7 @@
 class CommentsController < ApplicationController
   
   def create
-  @article = Article.find(params[:article_id])
-  
-  binding.pry
-  
+  @article = Article.find(params[:article_id])  
   @article.comments.create(comment_params)
   redirect_to article_path(@article)
   end
